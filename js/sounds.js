6187ac8f48d6cddfc163eb1af3404a7186a5ae64
celio <celiofarroco@gmail.com>
2023-04-11 16:59:45 -0300

    Reorganazing folders

--- js/sounds.js
+++ js/sounds.js
@@ -1,8 +1,8 @@
 export default function () {
-  const forestSound = new Audio("../assets/sounds/forest.wav");
-  const fireplaceSound = new Audio("../assets/sounds/fireplace.wav");
-  const rainSound = new Audio("../assets/sounds/rain.wav");
-  const coffeeshopSound = new Audio("../assets/sounds/coffeeshop.wav");
+  const forestSound = new Audio("../sounds/forest.wav");
+  const fireplaceSound = new Audio("../sounds/fireplace.wav");
+  const rainSound = new Audio("../sounds/rain.wav");
+  const coffeeshopSound = new Audio("../sounds/coffeeshop.wav");
 
   forestSound.loop = true;
   fireplaceSound.loop = true;
